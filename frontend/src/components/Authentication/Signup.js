f6c9eaca72c93648c53572062bf18aa162f58a6c
Anisha Pal <anisha.pal@rara.delivery>
2023-07-13 09:34:39 +0530

    feat: Integrate User signup page

--- frontend/src/components/Authentication/Signup.js
+++ frontend/src/components/Authentication/Signup.js
@@ -3,15 +3,15 @@ import { Button } from "@chakra-ui/button";
 import { FormControl, FormLabel } from "@chakra-ui/form-control";
 import { Input, InputGroup, InputRightElement } from "@chakra-ui/input";
 import { VStack } from "@chakra-ui/layout";
-// import { useToast } from "@chakra-ui/toast";
-// import axios from "axios";
-// import { useHistory } from "react-router";
+import { useToast } from "@chakra-ui/react";
+import axios from "axios";
+import { useHistory } from "react-router";
 
 const Signup = () => {
-  const [show, setShow] = useState(false);
-  //   const toast = useToast();
-  //   const history = useHistory();
+  const toast = useToast();
+  const history = useHistory();
 
+  const [show, setShow] = useState(false);
   const [name, setName] = useState();
   const [email, setEmail] = useState();
   const [confirmpassword, setConfirmpassword] = useState();
@@ -21,114 +21,114 @@ const Signup = () => {
 
   const handleClick = () => setShow(!show);
 
-  //   const submitHandler = async () => {
-  //     setPicLoading(true);
-  //     if (!name || !email || !password || !confirmpassword) {
-  //       toast({
-  //         title: "Please Fill all the Feilds",
-  //         status: "warning",
-  //         duration: 5000,
-  //         isClosable: true,
-  //         position: "bottom",
-  //       });
-  //       setPicLoading(false);
-  //       return;
-  //     }
-  //     if (password !== confirmpassword) {
-  //       toast({
-  //         title: "Passwords Do Not Match",
-  //         status: "warning",
-  //         duration: 5000,
-  //         isClosable: true,
-  //         position: "bottom",
-  //       });
-  //       return;
-  //     }
-  //     console.log(name, email, password, pic);
-  //     try {
-  //       const config = {
-  //         headers: {
-  //           "Content-type": "application/json",
-  //         },
-  //       };
-  //       const { data } = await axios.post(
-  //         "/api/user",
-  //         {
-  //           name,
-  //           email,
-  //           password,
-  //           pic,
-  //         },
-  //         config
-  //       );
-  //       console.log(data);
-  //       toast({
-  //         title: "Registration Successful",
-  //         status: "success",
-  //         duration: 5000,
-  //         isClosable: true,
-  //         position: "bottom",
-  //       });
-  //       localStorage.setItem("userInfo", JSON.stringify(data));
-  //       setPicLoading(false);
-  //       history.push("/chats");
-  //     } catch (error) {
-  //       toast({
-  //         title: "Error Occured!",
-  //         description: error.response.data.message,
-  //         status: "error",
-  //         duration: 5000,
-  //         isClosable: true,
-  //         position: "bottom",
-  //       });
-  //       setPicLoading(false);
-  //     }
-  //   };
+  const submitHandler = async () => {
+    setPicLoading(true);
+    if (!name || !email || !password || !confirmpassword) {
+      toast({
+        title: "Please Fill all the Feilds",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setPicLoading(false);
+      return;
+    }
+    if (password !== confirmpassword) {
+      toast({
+        title: "Passwords Do Not Match",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      return;
+    }
+    console.log(name, email, password, pic);
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+      const { data } = await axios.post(
+        "/api/user",
+        {
+          name,
+          email,
+          password,
+          pic,
+        },
+        config
+      );
+      console.log(data);
+      toast({
+        title: "Registration Successful",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      setPicLoading(false);
+      history.push("/chats");
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: error.response.data.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setPicLoading(false);
+    }
+  };
 
-  //   const postDetails = (pics) => {
-  //     setPicLoading(true);
-  //     if (pics === undefined) {
-  //       toast({
-  //         title: "Please Select an Image!",
-  //         status: "warning",
-  //         duration: 5000,
-  //         isClosable: true,
-  //         position: "bottom",
-  //       });
-  //       return;
-  //     }
-  //     console.log(pics);
-  //     if (pics.type === "image/jpeg" || pics.type === "image/png") {
-  //       const data = new FormData();
-  //       data.append("file", pics);
-  //       data.append("upload_preset", "chat-app");
-  //       data.append("cloud_name", "piyushproj");
-  //       fetch("https://api.cloudinary.com/v1_1/piyushproj/image/upload", {
-  //         method: "post",
-  //         body: data,
-  //       })
-  //         .then((res) => res.json())
-  //         .then((data) => {
-  //           setPic(data.url.toString());
-  //           console.log(data.url.toString());
-  //           setPicLoading(false);
-  //         })
-  //         .catch((err) => {
-  //           console.log(err);
-  //           setPicLoading(false);
-  //         });
-  //     } else {
-  //       toast({
-  //         title: "Please Select an Image!",
-  //         status: "warning",
-  //         duration: 5000,
-  //         isClosable: true,
-  //         position: "bottom",
-  //       });
-  //       setPicLoading(false);
-  //       return;
-  //     }
-  //   };
+  const postDetails = (pics) => {
+    setPicLoading(true);
+    if (pics === undefined) {
+      toast({
+        title: "Please Select an Image!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      return;
+    }
+    console.log(pics);
+    if (pics.type === "image/jpeg" || pics.type === "image/png") {
+      const data = new FormData();
+      data.append("file", pics);
+      data.append("upload_preset", "mern-chat-app-chatterly");
+      data.append("cloud_name", "dawgy004d");
+      fetch("https://api.cloudinary.com/v1_1/dawgy004d/image/upload", {
+        method: "post",
+        body: data,
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setPic(data.url.toString());
+          console.log(data.url.toString());
+          setPicLoading(false);
+        })
+        .catch((err) => {
+          console.log(err);
+          setPicLoading(false);
+        });
+    } else {
+      toast({
+        title: "Please Select an Image!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setPicLoading(false);
+      return;
+    }
+  };
 
   return (
     <VStack spacing="5px">
@@ -164,9 +164,9 @@ const Signup = () => {
       </FormControl>
       <FormControl id="pic">
         <FormLabel>Upload your Picture</FormLabel>
-        <Input type="file" p={1.5} accept="image/*" /*onChange={(e) => postDetails(e.target.files[0])}*/ />
+        <Input type="file" p={1.5} accept="image/*" onChange={(e) => postDetails(e.target.files[0])} />
       </FormControl>
-      <Button colorScheme="blue" width="100%" style={{ marginTop: 15 }} /*onClick={submitHandler}*/ isLoading={picLoading}>
+      <Button colorScheme="blue" width="100%" style={{ marginTop: 15 }} onClick={submitHandler} isLoading={picLoading}>
         Sign Up
       </Button>
     </VStack>
